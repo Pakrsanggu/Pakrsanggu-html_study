c653dfc682700e879b0f5bedfe4f6dfdb5f2383e
sanggupark <psg8906@naver.com>
2024-05-29 17:25:34 +0900

    Add files via upload

---
+++ 0529-3.js
@@ -0,0 +1,33 @@
+// //const $button = document.querySelector("#popup button");
+// //alert($button);
+// //function dat() {
+//    document.querySelector("#popup").style.display = "none"
+// }
+// $footer.addEventListener('click',function(){
+//     $section.style.display = "block";)
+
+// function bo(){document.querySelector("#popup").style.display = "block"
+// }
+
+// function blue(){
+//     $subMenu24.style.background = 'blue';
+// }//
+
+const menu1=document.querySelector('#AAA');
+menu1.addEventListener("click", BBB );
+
+function BBB(){
+    menu1.style.background="blue";
+}
+
+//id="sm41" onclick="tr()"
+//[서브메뉴4-1]을 클릭하면 글자색이 빨간색이 된다.
+const $sm41 = document.querySelector("#sm41");
+
+function tr() {
+    $sm41.style.color= 'red';
+    $sm41.style.textDecoration = 'none';
+}
+function tr(){
+    #sm41.style.color='red';
+}
